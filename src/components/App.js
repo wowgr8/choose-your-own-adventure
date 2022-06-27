d658fc54354a4a43be92322092effdf2c890ed2e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-26 18:46:28 -0700

    add initial file
    Co-authored-by: Wajma Niazi <WNiazi@users.noreply.github.com>
    Co-authored-by: Nick Sullivan <nicholassull@users.noreply.github.com>
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AdventureControl from './AdventureControl'
+
+function App() {
+  return(
+    <React.Fragment>
+      {/* <Header /> */}
+      <AdventureControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
