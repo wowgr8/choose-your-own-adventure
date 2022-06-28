ec8fbfb0be13d452a8db90604a6b98224c96ec7d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-27 20:56:29 -0700

    add useState and proper JSON path - WIP
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>
    Co-authored-by: Wajma Niazi <WNiazi@users.noreply.github.com>
    Co-authored-by: Nick Sullivan <nicholassull@users.noreply.github.com>

--- src/components/AdventureControl.js
+++ src/components/AdventureControl.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from "prop-types";
 // import Scene from "Scene";
 import { withFirestore, isLoaded } from 'react-redux-firebase';
+import firebase from 'firebase/compat/app';
 import {useSelector} from 'react-redux';  
 import {useFirestoreConnect} from 'react-redux-firebase';   
+// import { collection, query, where, getDocs } from "firebase/firestore";
+import { useState } from "react";
+
 
 
 function AdventureControl(props) {
@@ -11,27 +15,56 @@ function AdventureControl(props) {
   useFirestoreConnect([
     {collection: "chapter1"},
   ]);
-  // console.log("chapter1"); // 
 
-  const scenes = useSelector(state => state.firestore.ordered);
-  // console.log(scenes);
+  const db = firebase.firestore();
 
-  // scenes.chapter1.foreach(function(scene) {
-  //   console.log(scene.AltScene);
-  // })
+  const [chapter, setChapter ] = useState("chapter1");
+  const [sceneNumber, setSceneNumber ] = useState("1");
+  const [sceneText, setSceneText ] = useState("");
+
+
+
+  function changeScene() {
+    const randomNum = Math.floor(Math.random() * 3).toString();
+    var docRef = db.collection("chapter1").doc(randomNum);
+    docRef.get().then((doc) => {
+      console.log("Document data: ", doc.data().Scene);
+    });   
+  }
+
+//changeScene(chapter, sceneNumber);
+//handleOptionOne =(id)=> {
+// this.props.firestone.get
+//}
+
   return (
     <div>
       {/* <p>{this.props.firestore.get({collection: "chapter 1", doc: "1"})}</p> */}
       <p>Does this show?</p>
-      {/* <h3>{scenes.AltScene}</h3> */}
-      {/* <p>{getSelectedScene}</p>  */}
+
+      {/* currentVisibleState =<Scene onSceneSelection ={ 
+        scene = {this.state.selectedScene} 
+        onClickOptionOne ={ this.handleOptionOne} 
+        onClickOptionTwo= {this.handleOptionTwo} */} 
     </div>
   )
 }
 
-{/* this.props.firestore.get({collection: "chapter1", doc: id}).then((chapter1) => {  
-      const firestoreChapter1 = {           
-        location: chapter1.get("descriptions"), 
-        options: chapter1.get("options),
-        id: chapter1.id */}
+
 export default withFirestore(AdventureControl);
+
+//onClick  () { 
+//  this.setState( state =>{( 
+// scene : random (db.collection(chapter).doc(scene))
+//)})
+//}
+
+// function of getNewScenes
+// if one is choosen then go to db.scene.river
+// if two is choose then got to db ....desert
+
+//function changeScene 
+//  const [sceneNumber, setSceneNumber ] = useState("1");
+//return  ( 
+  //<button> onClick ()=> setSceneNumber({randomizedNumber})) </button>
+//)//setScene (()=> getNewScene)
